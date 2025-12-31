7aab93b5a04cd6e27a1ca36310119bc5fa8c97bf
Sadeepa Dinakara <sadeepa14dinakara@gmail.com>
2025-12-31 09:15:09 +0530

    user controller created

---
+++ Backend/src/controllers/userController.js
@@ -0,0 +1,28 @@
+const user = require("../modules/usermodle");
+
+const rejisteruser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || email || password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+    const userexist = await user.findOne({ email });
+    if (userexist) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await user.create({ name, email, password });
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+      });
+    } else {
+      res.status(400).json({ message: "invaild user details" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { rejisteruser };
